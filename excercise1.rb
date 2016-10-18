505f94e5b7b3a6a1a2d8c01308fbe70e4be0d0e3
rathesh.richard@gmail.com <rathesh.richard@gmail.com>
2016-10-18 13:10:25 -0400

    Excercise 1 complete

---
+++ excercise1.rb
@@ -0,0 +1,25 @@
+puts 2
+puts 3
+puts 2 + 3
+
+# Escape Sequences
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+#booleans
+(1 < 3) && (3 < 5)
+# true
+
+(1 > 1) && (2 > 2)
+# false
+
+(1 == 2) || (2 < 3)
+# true
+
+(1 != 2) || (2 < 3)
+# true
+
+(1 == 2) || (2 == 3)
+#false

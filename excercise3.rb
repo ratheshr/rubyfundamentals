48eda69fe7019bb7c18e9308f5d5fa42e060e618
rathesh.richard@gmail.com <rathesh.richard@gmail.com>
2016-10-18 13:19:28 -0400

    Finished excercise 3 with user's birth year

--- excercise3.rb
+++ excercise3.rb
@@ -1,1 +1,13 @@
+#excercise for getting a persons name and age
+
 puts "what is your name?"
+name = gets.chomp
+
+puts"Hi, #{name}!"
+puts "How old are you?"
+age = gets.chomp.to_i
+
+current_year = Time.now.year
+birth_year = (current_year - age)
+
+puts "Thanks, you were born in #{birth_year}"
